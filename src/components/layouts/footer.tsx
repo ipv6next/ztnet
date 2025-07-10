daebcc60256e04631b34e022e2be60f96080492c
ipv6next <135030220+ipv6next@users.noreply.github.com>
2025-07-11 05:27:05 +0800

    Update footer.tsx

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => (
 		<div className="grid grid-cols-3">
 			<div></div>
 			<p className="text-center text-sm text-gray-600">
-				© Kodea Solutions {new Date().getFullYear()}. All rights reserved
+				© ipv6next Solutions {new Date().getFullYear()}. All rights reserved
 			</p>
 			{globalSiteVersion ? (
 				<p className="text-right text-sm text-gray-600">{globalSiteVersion}</p>
